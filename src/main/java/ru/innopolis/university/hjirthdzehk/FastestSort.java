9b2a48787227dec62436ea4ef61cdd71e3644449
Anton Kiselev <straw.berry.pie@ya.ru>
2016-09-15 17:40:32 +0300

    Counting sort.

--- src/main/java/ru/innopolis/university/hjirthdzehk/FastestSort.java
+++ src/main/java/ru/innopolis/university/hjirthdzehk/FastestSort.java
@@ -3,84 +3,45 @@ package ru.innopolis.university.hjirthdzehk;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class FastestSort {
 
-    private static boolean isSorted(double[] data) {
-        for (int i = 1; i < data.length; ++i) {
-            double left = data[i - 1];
-            double right = data[i];
-            if (left > right) {
-                return false;
-            }
-        }
-        return true;
+    private static int convertTo(double value) {
+        return (int) Math.round(value * 1000000 + 500000000);
     }
 
-    private static boolean isSortedReverse(double[] data) {
-        for (int i = 1; i < data.length; ++i) {
-            double left = data[i - 1];
-            double right = data[i];
-            if (left < right) {
-                return false;
-            }
-        }
-        return true;
+    private static double convertFrom(int value) {
+        return (value - 500000000) / 1000000.0;
     }
 
-    public static void sort(double[] data) {
-        Arrays.sort(data);
-    }
+    public static double[] countingSort(double[] input, int bucketsCount) {
+        byte[] counts = new byte[bucketsCount];
+        Arrays.stream(input).forEach(v -> counts[convertTo(v)]++);
 
-    private static double[] bucketSort(double[] data, int bucketsCount) {
-
-        int lowerBound = -500;
-        int upperBound = 500;
-
-        List<List<Double>> buckets = new ArrayList<>(bucketsCount);
-        for (int i = 0; i < bucketsCount; ++i) {
-            buckets.add(new ArrayList<>(data.length / bucketsCount));
-        }
-
-        for (double currentElement : data) {
-            assert currentElement >= lowerBound && currentElement <= upperBound;
-            int bucketIndex = (int) Math.floor(bucketsCount * currentElement / (2 * upperBound) + bucketsCount / 2); // TODO might not work for other bounds
-            buckets.get(bucketIndex).add(currentElement);
-        }
-
-        double[] result = new double[data.length];
-        int currentPosition = 0;
-
-        for (List<Double> bucket : buckets) {
-            double[] temp = new double[bucket.size()];
-            for (int i = 0; i < bucket.size(); ++i) {
-                temp[i] = bucket.get(i);
-            }
-            Arrays.sort(temp); // todo sort in parallel
-            for (int i = 0; i < bucket.size(); ++i) {
-                result[currentPosition] = temp[i];
-                currentPosition++;
+        int currentInsertPos = 0;
+        for (int currentBucket = 0; currentBucket < bucketsCount; ++currentBucket) {
+            if (counts[currentBucket] == 0) continue;
+            for (byte i = 0; i < counts[currentBucket]; ++i) {
+                input[currentInsertPos++] = convertFrom(currentBucket);
             }
         }
 
-        return result;
-
+        return input;
     }
 
     public static void main(String[] args) throws IOException {
+
         double[] values = readValues("./data.txt");
+
         System.out.println("Started!");
-        double[] result = bucketSort(values, 50000);
-        Arrays.sort(values);
+        double[] result = countingSort(values, 1000000000);
         System.out.println("Done!");
     }
 
     private static double[] readValues(String filename) throws IOException {
         return Files.lines(Paths.get(filename)).parallel().
-                mapToDouble(Float::parseFloat).toArray();
+                mapToDouble(Double::parseDouble).toArray();
     }
 
 }
